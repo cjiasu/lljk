ecc65152397e1fbb9de28fce9604c1dd8ba08f09
cjiasu <40857324+cjiasu@users.noreply.github.com>
2019-06-05 15:02:45 +0800

    Update iptables.sh

--- jk/iptables.sh
+++ jk/iptables.sh
@@ -1,3 +1,4 @@
+
 #!/bin/sh
 
 local_ip="10.0.0.4"
@@ -11,8 +12,8 @@ get_ip=`ping -c 1 $d_domain | grep 'PING' | awk '{print $3}' | sed 's/[(,)]//g'`
 
 cd `dirname $0`
 
-if [ -e './$l_log.txt' ]; then
-	old_ip=`tail ./$l_log.txt -n 1`
+if [ -e './ip1.txt' ]; then
+	old_ip=`tail ./ip1.txt -n 1`
 	if ! [ "$old_ip" = "$get_ip"  ]; then
 	`/sbin/iptables -t nat -D PREROUTING -d $local_ip/32 -p tcp -m tcp --dport $s_port -j DNAT --to-destination $old_ip:$d_port`
 	`/sbin/iptables -t nat -D POSTROUTING -d $old_ip/32 -p tcp -m tcp --dport $d_port -j SNAT --to-source $local_ip`
@@ -24,12 +25,12 @@ if [ -e './$l_log.txt' ]; then
         `/sbin/iptables -t nat -A PREROUTING -d $local_ip/32 -p udp -m udp --dport $s_port -j DNAT --to-destination $get_ip:$d_port`
 	`/sbin/iptables -t nat -A POSTROUTING -d $get_ip/32 -p udp -m udp --dport $d_port -j SNAT --to-source $local_ip`
 
-		echo $get_ip >> ./$l_log.txt
+		echo $get_ip >> ./ip1.txt
 	fi
 else
-	echo $get_ip >> ./$l_log.txt
+	echo $get_ip >> ./ip1.txt
 	`/sbin/iptables -t nat -A PREROUTING -d $local_ip/32 -p tcp -m tcp --dport $s_port -j DNAT --to-destination $get_ip:$d_port`
 	`/sbin/iptables -t nat -A POSTROUTING -d $get_ip/32 -p tcp -m tcp --dport $d_port -j SNAT --to-source $local_ip`
-        `/sbin/iptables -t nat -A PREROUTING -d $local_ip/32 -p udp -m udp --dport $s_port -j DNAT --to-destination $get_ip:$d_port`
-        `/sbin/iptables -t nat -A POSTROUTING -d $get_ip/32 -p udp -m udp --dport $d_port -j SNAT --to-source $local_ip`	
+    `/sbin/iptables -t nat -A PREROUTING -d $local_ip/32 -p udp -m udp --dport $s_port -j DNAT --to-destination $get_ip:$d_port`
+    `/sbin/iptables -t nat -A POSTROUTING -d $get_ip/32 -p udp -m udp --dport $d_port -j SNAT --to-source $local_ip`	
 fi
